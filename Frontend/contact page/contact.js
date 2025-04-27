f7a95c95a834139694d172270b6596c807c11a66
Devkant Nayak <devakanta0121.becse24@chitkara.edu.in>
2025-04-27 08:32:28 +0530

    some chages in the page contact.js

---
+++ Frontend/contact page/contact.js
@@ -0,0 +1,6 @@
+    // Simple form submission handler
+    document.getElementById('contactForm').addEventListener('submit', function(e) {
+      e.preventDefault();
+      alert('Thank you for contacting us! We will get back to you soon.');
+      this.reset();
+    });
